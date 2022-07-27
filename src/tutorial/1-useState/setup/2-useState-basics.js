dfb23eeb6adfcede03754a22de9e18cb0ecb9bd9
Nambu <86805124+luiznambu@users.noreply.github.com>
2022-07-27 19:02:44 -0300

    adicao de comentarios

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -4,16 +4,16 @@ const UseStateBasics = () => {
   // const value = useState(1)[0]
   // const handler = useState(1)[1]
   // console.log(value, handler);
-  const [text,setText] = useState('random title')
+  const [text,setText] = useState('random title') // "text" é o estado original e "setText" a funcao que irá mudar o texto dentro do useState
   const handleClick = () => {
-    if(text === 'random title'){
+    if(text === 'random title'){  //condicao para mudar cada vez que apertar o botao
       setText('Hello World!');
     } else {
       setText("random title");
     }
     
   }
-  return (
+  return (  // botao que ativa o "handleClick" que eh a funcao que ativa o "setText"
     <React.Fragment>
       <h1> {text} </h1>
       <button className="btn" onClick={handleClick}>Change Title</button>
